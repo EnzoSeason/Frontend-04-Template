53fcc50d720b71e412873731eada6685ecab6da3
Jijie LIU <liujijieseason@gmail.com>
2020-10-04 20:22:58 +0200

    init ResponseParser

--- Week_06/src/client.js
+++ Week_06/src/client.js
@@ -26,9 +26,9 @@ class Resquest {
     }
 
     toString() {
-        return `${this.method} ${this.path} HTTP/1.1\n${Object.keys(this.header)
+        return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.header)
             .map(key => `${key}: ${this.header[key]}`)
-            .join('\n')}\n\n${this.bodyText}`;
+            .join('\r\n')}\r\n\r\n${this.bodyText}`;
     }
 
     send(connection) {
@@ -55,6 +55,7 @@ class Resquest {
             });
             connection.on('error', (error) => {
                 console.log(error);
+                reject(error);
                 connection.end();
             })
         });
@@ -62,9 +63,77 @@ class Resquest {
 }
 
 class ResponseParser {
-    constructor() { }
-    receive(string) {
+    constructor() { 
+        this.WAITING_STATUS_LINE = 0
+		this.WAITING_STATUS_LINE_END = 1
+		this.WAITING_HEADER_NAME = 2
+		this.WAITING_HEADER_SPACE = 3
+		this.WAITING_HEADER_VALUE = 4
+		this.WAITING_HEADER_LINE_END = 5
+		this.WAITING_HEADER_BLOCK_END = 6
+		this.WAITING_BODY = 7
 
+		this.current = this.WAITING_STATUS_LINE
+		this.statusLine = ''
+		this.headers = {}
+		this.headerName = ''
+		this.headerValue = ''
+		this.bodyParser = null
+    }
+    
+    receive(string) {
+        for (let i = 0; i < string.length; i++) {
+            this.receiveChar(string.charAt(i));
+        }
+    }
+
+    /**
+     * use finite-state-machine to read the response
+     * @param {string} char 
+     */
+    receiveChar(char) {
+        if (this.current === this.WAITING_STATUS_LINE) {
+			if (char === '\r') {
+				this.current = this.WAITING_STATUS_LINE_END
+			} else {
+				this.statusLine += char
+			}
+		} else if (this.current === this.WAITING_STATUS_LINE_END) {
+			if (char === '\n') {
+				this.current = this.WAITING_HEADER_NAME
+			}
+		} else if (this.current === this.WAITING_HEADER_NAME) {
+			if (char === ':') {
+				this.current = this.WAITING_HEADER_SPACE
+			} else if (char === '\r') {
+				this.current = this.WAITING_HEADER_BLOCK_END
+			} else {
+				this.headerName += char
+			}
+		} else if (this.current === this.WAITING_HEADER_SPACE) {
+			if (char === ' ') {
+				this.current = this.WAITING_HEADER_VALUE
+			}
+		} else if (this.current === this.WAITING_HEADER_VALUE) {
+			if (char === '\r') {
+				this.current = this.WAITING_HEADER_LINE_END
+				this.headers[this.headerName] = this.headerValue
+				this.headerName = ''
+				this.headerValue = ''
+			} else {
+				this.headerValue += char
+			}
+		} else if (this.current === this.WAITING_HEADER_LINE_END) {
+			if (char === '\n') {
+				this.current = this.WAITING_HEADER_NAME
+			}
+		} else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+			if (char === '\n') {
+				this.current = this.WAITING_BODY
+			}
+		} else if (this.current === this.WAITING_BODY) {
+            console.log(char)
+		}
     }
 }
 
