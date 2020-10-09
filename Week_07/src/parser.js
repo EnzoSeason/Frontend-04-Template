a99373c9e33a8eed15b32b1882d115d2ddbd41c4
Jijie LIU <liujijieseason@gmail.com>
2020-10-09 17:29:14 +0200

    init fms to read html

--- Week_07/src/parser.js
+++ Week_07/src/parser.js
@@ -1,5 +1,13 @@
 const EOF = Symbol("EOF");
 
+function data(c) {
+    
+}
+
 module.exports.parseHTML = function parseHTML(html) {
-    console.log(html);
+    let state = data;
+    for (let c of html) {
+        state = data(c);
+    }
+    state = state(EOF);
 }
