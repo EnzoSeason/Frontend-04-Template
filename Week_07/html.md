2351dcb61bdb97fe556efa5237ce246a660cac49
Jijie LIU <liujijieseason@gmail.com>
2020-10-09 18:03:09 +0200

    start html analyze

--- Week_07/html.md
+++ Week_07/html.md
@@ -1,3 +1,10 @@
 # HTML解析
 
-* [code](./src/parser.js)
+使用有限状态机解析标签。
+
+有三种标签：
+* 开始标签
+* 结束标签
+* 自封标签
+
+[code](./src/parser.js)
--- Week_07/src/parser.js
+++ Week_07/src/parser.js
@@ -1,13 +1,106 @@
 const EOF = Symbol("EOF");
 
-function data(c) {
+function start(c) {
+    if (c === "<") {
+        return tagOpen;
+    }
     
+    if (c === EOF) {
+        return;
+    } 
+    
+    return start;
+}
+
+function tagOpen(c) {
+    // </
+    if (c === "/") {
+        return endTagOpen;
+    }
+
+    // <p 
+    if (c.match(/^[a-zA-Z]$/)) {
+        return tagName(c);
+    }
+
+    return;
+}
+
+function endTagOpen(c) {
+    // </p
+    if (c.match(/^[a-zA-Z]$/)) {
+        return tagName(c);
+    }
+
+    // </>, error
+    if (c === ">") {
+
+    }
+
+    // </[EOF], error
+    if (c === EOF) {
+
+    }
+
+    // other errors
+}
+
+function tagName(c) {
+    // space
+    if (c.match(/^[\t\n\f\s]$/)) {
+        return beforeAttributeName;
+    }
+
+    if (c === "/") {
+        return selfClosingStartTag;
+    }
+
+    if (c === ">") {
+        return start;
+    }
+
+    if (c.match(/^[a-zA-Z]$/)) {
+        return tagName;
+    }
+
+    return tagName;
+}
+
+function beforeAttributeName(c) {
+    // space
+    if (c.match(/^[\t\n\f\s]$/)) {
+        return beforeAttributeName;
+    }
+
+    if (c === ">") {
+        return start;
+    }
+
+    if (c === "=") {
+        return beforeAttributeName;
+    }
+
+    return beforeAttributeName;
+}
+
+function selfClosingStartTag(c) {
+    if (c === ">") {
+        currentToke.isSelfClosing = true;
+        return start;
+    }
+
+    // error
+    if (c === EOF) {
+
+    }
+
+    // other errors
 }
 
 module.exports.parseHTML = function parseHTML(html) {
-    let state = data;
+    let state = start;
     for (let c of html) {
-        state = data(c);
+        state = state(c);
     }
     state = state(EOF);
 }
