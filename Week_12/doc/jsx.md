ed380f62d97ee9d5f9355312a20de52a87f2835e
Jijie LIU <liujijieseason@gmail.com>
2020-11-16 23:21:31 +0100

    finish jsx

--- Week_12/doc/jsx.md
+++ Week_12/doc/jsx.md
@@ -10,6 +10,31 @@
 
 4. `npm install --save-dev @babel/core @babel/preset-env`
 
-5. 创建`webpack.config.js`
+5. `npm install --save-dev @babel/plugin-transform-react-jsx`
+
+创建[webpack.config.js](../jsx/webpack.config.js)
 
 webpack用来打包文件，babel用来转译不同版本的JS
+
+## 理解JSX
+
+* work space: [main.js](../jsx/main.js)
+* result: [main.html](../jsx/dist/main.html)
+
+### Version 1：只用HTML元素
+
+[main basic](../jsx/main-basic.js)
+
+使用DOM API，创建`createElement`函数
+
+利用递归（调用appendChild），搭建DOM树
+
+### Version 2: 自定义元素
+
+[main](../jsx/main.js)
+
+基于Version 1，创建`Wrapper`, 代理DOM API。
+
+优点|缺点
+---|---
+可以自定义元素|每次递归（调用appendChild）都要从叶子节点开始，重建DOM树，性能一般
--- Week_12/jsx/main.js
+++ Week_12/jsx/main.js
@@ -1,3 +1,69 @@
-for (let i of [1,2,3]) {
-    console.log(i);
+function createElement(type, attributes, ...children) {
+    let element;
+    if (typeof type === "string") {
+        // wrap the native HTML element
+        element = new ElementWrapper(type);
+    } else {
+        element = new type;
+    }
+    for (const name in attributes) {
+        element.setAttribute(name, attributes[name]);
+    }
+    for (const child of children) {
+        if (typeof child === 'string') {
+            const textNode = new TextNodeWrapper(child);
+            element.appendChild(textNode); // appendChild must have used mountTo
+        } else {
+            element.appendChild(child); // appendChild must have used mountTo
+        }
+    }
+    return element;
 }
+
+class Wrapper {
+    constructor(type, element) {
+        if (type === 'element') {
+            this.root = document.createElement(element);
+        }
+        if (type === 'text') {
+            this.root = document.createTextNode(element);
+        }
+    }
+    mountTo(parent) {
+        // proxy of DOM API: appendChild
+        parent.appendChild(this.root);
+    }
+    appendChild(child) {
+        // use proxy: mountTo
+        child.mountTo(this.root);
+    }
+    setAttribute(name, value) {
+        // DOM API
+        this.root.setAttribute(name, value);
+    }
+}
+class TextNodeWrapper extends Wrapper{
+    constructor(content) {
+        super('text', content);
+    }
+}
+
+class ElementWrapper extends Wrapper{
+    constructor(type){
+        super('element', type);
+    }
+}
+
+class MyDiv extends Wrapper{
+    constructor() {
+        super('element', 'div');
+    }
+}
+
+const a = <MyDiv id="a" class="cls">
+    <span>Hello</span>
+    <br />
+    <span>Bonjour</span>
+</MyDiv>;
+
+a.mountTo(document.body);
