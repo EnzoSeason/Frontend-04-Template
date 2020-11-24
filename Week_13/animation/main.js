302e901cca4e386246cc49e06b8bf8cd84c185a2
Jijie LIU <liujijieseason@gmail.com>
2020-11-24 15:02:12 +0100

    Update main.js

--- Week_13/animation/main.js
+++ Week_13/animation/main.js
@@ -5,7 +5,7 @@ let tl = new Timeline();
 let animation = new Animation(
     document.querySelector('#el').style,
     "transform",
-     0, 500, 2000, 0,
+     0, 500, 2000, 1000,
      bezier(.25,.1,.25,1), v => `translateX(${v}px)`
 );
 
@@ -13,6 +13,4 @@ document.querySelector('#el').addEventListener('mouseover', () => tl.pause());
 document.querySelector('#el').addEventListener('mouseout', () => tl.resume());
 
 tl.start();
-setTimeout(() => {
-    tl.add(animation);
-}, 1000);
+tl.add(animation);
