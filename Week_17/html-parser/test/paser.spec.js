a4bcd920c692e375accf458bfcc53f58ada49b4f
Jijie LIU <liujijieseason@gmail.com>
2020-12-21 21:24:35 +0100

    config vsc launch and enable source map

--- Week_17/html-parser/test/paser.spec.js
+++ Week_17/html-parser/test/paser.spec.js
@@ -2,9 +2,16 @@ import assert from 'assert'
 import parseHTML from '../src/parser.js';
 
 describe("parse HTML", function() {
-    it('<a>abc</a>', function() {
-        const dom = parseHTML('<a>abc</a>');
-        assert.strictEqual(1, 1);
+    it('<a></a>', function() {
+        const tree = parseHTML('<a></a>');
+        assert.strictEqual(tree.children[0].tagName, 'a');
+        assert.strictEqual(tree.children[0].children.length, 0);
+    })
+    it('<a href="/"></a>', function() {
+        const tree = parseHTML('<a href="/"></a>');
+        console.log(tree);
+        assert.strictEqual(tree.children[0].tagName, 'a');
+        assert.strictEqual(tree.children[0].children.length, 0);
     })
 })
 
